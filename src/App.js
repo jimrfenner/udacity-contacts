42fe36a799b741f78535e90fec8fa1dc4f995bc4
Jim Fenner <jimrfenner@gmail.com>
2018-01-02 19:28:10 -0500

    Created shell for CreateContact component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
@@ -27,6 +28,7 @@ class App extends Component {
           onDeleteContact={this.removeContact}
           contacts={this.state.contacts}
         />
+        <CreateContact />
       </div>
     );
   }
